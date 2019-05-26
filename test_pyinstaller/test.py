40eb92e0f60fc642de610c4f58336bc810144dee
LtGlahn <jensen72@gmail.com>
2019-05-26 08:30:59 +0200

    Tester bruk av ferdigkompilerte *.exe-filer

---
+++ test_pyinstaller/test.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import time
+import json
+import sys
+
+def hoved( arg1='dummy'): 
+    print( "Hello, world")
+    print( 'Arg 1', arg1) 
+    time.sleep( 1)
+    
+    
+if __name__ == "__main__":
+    inputfil = 'testinput.json' 
+    if len( sys.argv) > 1: 
+        inputarg = sys.argv[1]
+    
+
+        # Leser oppsett fra *.json-fil    
+        if '.json' in inputfil[-5:].lower():
+            with open( inputarg) as f: 
+                myinput = json.load( f) 
+               
+            if 'arg1' in myinput.keys(): 
+                print( "Kjører kommando:\nhoved(", myinput['arg1'], ')'  )
+                hoved( arg1=myinput['arg1']) 
+                
+        else: 
+            hoved( inputarg )
+       
+    else: 
+        
+        print( "Test av hvordan vi bruker kommandolinje-argumenter ")
+        print( "for python-kode kompilert til windows-kjørbare filer (*.exe)")
+        print( "To muligheter: ")
+        print( "")
+        print( "  1. Angi filnavn (*.json) for oppsettfil med nødvendige argumenter")
+        print( "  ")
+        print( "        test.exe oppsettfil_test.json")
+        print( "  ")
+        print( "  2. Angi argumentene direkte fra kommandonlinjen ")
+        print( "        ")
+        print( '        test.exe "argument 1 (typisk langt og kronglete mappenavn" ')
+        print( "")
+        print( "")
